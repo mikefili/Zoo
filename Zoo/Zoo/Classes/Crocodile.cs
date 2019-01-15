328e4dee111ea3d21375dec1525fdb13ac9294e5
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-15 15:12:29 -0800

    Crocodile class complete

--- Zoo/Zoo/Classes/Crocodile.cs
+++ Zoo/Zoo/Classes/Crocodile.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace Zoo.Classes
 {
-    class Crocodile
+    public abstract class Crocodile : Reptile
     {
+        public override string diet { get => base.diet; set => base.diet = "carnivore"; }
+        public override bool shell { get => base.shell; set => base.shell = false; }
+        public override bool tail { get => base.tail; set => base.tail = true; }
+
+        public abstract void BirdTeethClean();
+        public override void Eat()
+        {
+            Console.WriteLine("I like to eat meat!!!");
+        }
+        public override void Sleep()
+        {
+            Console.WriteLine("ZZZ... ZZZ... ZZZ... I like to sleep.");
+        }
     }
 }

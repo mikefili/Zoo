5a6bf50de8d3017d4ecf03ae068b7e63ffebc332
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-15 15:17:10 -0800

    PolarBear class complete

--- Zoo/Zoo/Classes/PolarBear.cs
+++ Zoo/Zoo/Classes/PolarBear.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace Zoo.Classes
 {
-    class PolarBear
+    public abstract class PolarBear : Bear
     {
+        public override string color { get => base.color; set => base.color = "white"; }
+        public override bool swims { get => base.swims; set => base.swims = true; }
+        public override bool tail { get => base.tail; set => base.tail = true; }
+
+        public abstract void Swim();
+        public override void Eat()
+        {
+            Console.WriteLine("I like to eat meat!!!");
+        }
+        public override void Sleep()
+        {
+            Console.WriteLine("ZZZ... ZZZ... ZZZ... I like to sleep.");
+        }
     }
 }

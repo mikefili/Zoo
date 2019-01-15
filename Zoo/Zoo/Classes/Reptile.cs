8cd890798f3beeb42ee69f4bdd5ffab2a26eb749
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-15 15:10:29 -0800

    Tortise class complete

--- Zoo/Zoo/Classes/Reptile.cs
+++ Zoo/Zoo/Classes/Reptile.cs
@@ -8,7 +8,7 @@ namespace Zoo.Classes
     {
         public override bool tail { get => base.tail; set => base.tail = true; }
         public virtual bool shell { get; set; }
-        public abstract int lifespan { get; set; }
+        public virtual int lifespan { get; set; }
 
         public abstract void Hunt();
         public abstract bool Swim();

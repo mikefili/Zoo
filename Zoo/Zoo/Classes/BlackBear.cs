630eb83f3bd9f2d6b4bc070be0d25cbfd5aded62
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-15 15:18:35 -0800

    BlackBear class complete. All classes complete.

--- Zoo/Zoo/Classes/BlackBear.cs
+++ Zoo/Zoo/Classes/BlackBear.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace Zoo.Classes
 {
-    class BlackBear
+    public abstract class BlackBear : Bear
     {
+        public override string color { get => base.color; set => base.color = "black"; }
+        public override bool swims { get => base.swims; set => base.swims = false; }
+        public override bool tail { get => base.tail; set => base.tail = true; }
+
+        public abstract void ClimbTree();
+        public override void Eat()
+        {
+            Console.WriteLine("I like to eat meat!!!");
+        }
+        public override void Sleep()
+        {
+            Console.WriteLine("ZZZ... ZZZ... ZZZ... I like to sleep.");
+        }
     }
 }
